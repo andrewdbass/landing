131fb50a96145151054e7e433a7f5acd6dc2532a
andrewdbass <andybass12@gmail.com>
2015-10-12 18:17:09 -0600

    update prod environment send grid stuff 2.0

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = {:host => 'https://landingpagetest.herokuapp.com/'}
 
-  config.action_mailer.deliver_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
     port: "587",
